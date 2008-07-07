188704545f41cf1359d5974df0c622a7a9016b54
yasunobu13 <yasunobu13@194bc0c5-1251-0410-adcc-b97d8ca696f6>
2008-07-07 03:56:34 +0000

    Created Math namespace and Real type with helpers

---
+++ trunk/bitwise-engine/Math/Real.h
@@ -0,0 +1,31 @@
+#pragma once
+
+namespace BitwiseEngine
+{
+
+namespace Math
+{
+
+#ifdef BE_REAL_DOUBLE
+typedef double Real
+#else
+typedef float Real
+#endif
+
+class RealHelper
+{
+public:
+	static float ToFloat(Real r);
+	static double ToDouble(Real r);
+
+	static Real ToReal(float f);
+	static Real ToReal(double d);
+
+	static Real ZERO;
+	static Real ONE;
+
+};
+
+};
+
+};
